f78dd2d38056179128f5ea52207eb6d39a8e6a53
Benjamin Goering <171782+gobengo@users.noreply.github.com>
2022-07-05 16:24:42 -0700

    IndexerMetricsCollector keeps track of metrics.indexingDurationSeconds

--- src/indexer-metrics-collector/indexer-metrics-collector.spec.js
+++ src/indexer-metrics-collector/indexer-metrics-collector.spec.js
@@ -11,6 +11,9 @@ import assert from "node:assert";
 import { DurableObjectStorage } from "@miniflare/durable-objects";
 import { MemoryStorage } from "@miniflare/storage-memory";
 
+const exampleImageUri =
+  "https://bafkreigh2akiscaildcqabsyg3dfr6chu3fgpregiymsck7e7aqa4s52zy.ipfs.nftstorage.link/";
+
 test("can send IndexerCompleted event to IndexerMetricsCollector", async (t) => {
   const storage = new DurableObjectStorage(new MemoryStorage());
   const collector = new IndexerMetricsCollector(storage);
@@ -110,4 +113,47 @@ test("can send event multiple IndexerNotified events to multiple IndexerMetricsC
   assert.ok(ipfsIndexerNotified?.type === "HISTOGRAM");
   t.is(ipfsIndexerNotified?.metrics.length, 1);
   t.is(ipfsIndexerNotified?.metrics[0].count, "2");
+});
+
+test("can send event multiple IndexerCompleted events to multiple IndexerMetricsCollector and then request indexing_duration_seconds metrics", async (t) => {
+  const storage = new DurableObjectStorage(new MemoryStorage());
+  const collector1 = new IndexerMetricsCollector(storage);
+  const now = new Date();
+  const oneMinuteFromNow = new Date(Number(now) + 60 * 1000);
+  /** @type {IndexerCompleted} */
+  const indexerCompletedEvent = new IndexerCompleted(exampleImageUri, 1e6, {
+    startTime: now,
+    endTime: oneMinuteFromNow,
+  });
+  // submit an event1
+  const eventSubmissionResponse1 = await collector1.fetch(
+    new Request("https://example.com/events", {
+      method: "post",
+      body: JSON.stringify(indexerCompletedEvent),
+      headers: {
+        "content-type": "application/json",
+      },
+    })
+  );
+  t.is(eventSubmissionResponse1.status, 202);
+
+  // fetch metrics
+  const collector2 = new IndexerMetricsCollector(storage);
+  const metricsResponse = await collector2.fetch(
+    new Request("https://example.com/metrics")
+  );
+  t.is(metricsResponse.status, 200);
+  const metricsResponseText = await metricsResponse.text();
+  t.assert(
+    metricsResponseText.toLowerCase().includes("histogram"),
+    "expected metrics response text to contain histogram"
+  );
+  const parsedMetrics = parsePrometheus(metricsResponseText);
+  const indexingDuration = parsedMetrics.find(
+    (m) => m.name === "indexing_duration_seconds"
+  );
+  t.assert(indexingDuration);
+  assert.ok(indexingDuration?.type === "HISTOGRAM");
+  t.is(indexingDuration?.metrics.length, 1);
+  t.is(indexingDuration?.metrics[0].count, "1");
 });
