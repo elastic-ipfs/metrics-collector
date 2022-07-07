e0870378205e62a7fabddfd52227057938e1ca3c
Benjamin Goering <171782+gobengo@users.noreply.github.com>
2022-07-06 18:56:39 -0700

    Add test that fails in miniflare when doing ajv.compile

--- src/indexer-metrics-collector/worker.spec.js
+++ src/indexer-metrics-collector/worker.spec.js
@@ -2,6 +2,7 @@ import test from "ava";
 import { Miniflare } from "miniflare";
 import { dirname, join } from "path";
 import { fileURLToPath } from "url";
+import { IndexerNotified } from "../indexer-events/indexer-events.js";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
@@ -26,4 +27,19 @@ test("can test indexer-metrics-collector with miniflare", async (t) => {
   const res = await mf.dispatchFetch("http://localhost:8787");
   // Check the count is "1" as this is the first time we've been to this path
   t.is(await res.text(), "indexer-metrics-collector");
+
+  // test sending an event
+  const eventSubmissionResponse = await mf.dispatchFetch(
+    "http://localhost:8787/events",
+    {
+      method: "post",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(
+        new IndexerNotified("https://dag.house", 1, new Date())
+      ),
+    }
+  );
+  t.is(eventSubmissionResponse.status, 202);
 });
