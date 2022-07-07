f09ee430281008101ac6e36e0a1cde913232bb20
Benjamin Goering <171782+gobengo@users.noreply.github.com>
2022-07-07 11:50:46 -0700

    Use @cfworker/json-schema instead of ajv for schema validation, resolves #17

--- src/indexer-events/indexer-events.spec.js
+++ src/indexer-events/indexer-events.spec.js
@@ -1,6 +1,13 @@
 import { isValid } from "../schema.js";
 import { test } from "../testing.js";
 import { IndexerCompleted, IndexerNotified } from "./indexer-events.js";
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
+import * as fs from "fs/promises";
+import { hasOwnProperty } from "../object.js";
+import * as assert from "assert";
+import * as events from "./indexer-events.js";
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const exampleImageUri =
   "https://bafkreigh2akiscaildcqabsyg3dfr6chu3fgpregiymsck7e7aqa4s52zy.ipfs.nftstorage.link/";
@@ -64,4 +71,22 @@ test("can validate an IndexerCompleted event", (t) => {
     ),
     true
   );
+});
+
+test("indexer-events examples are all valid", async (t) => {
+  const examplesDir = join(__dirname, "examples");
+  for (const file of await fs.readdir(examplesDir)) {
+    const json = /** @type {unknown } */ (
+      await fs.readFile(join(examplesDir, file), { encoding: "utf-8" })
+    );
+    const parsed = /** @type {unknown} */ (JSON.parse(String(json)));
+    assert.ok(hasOwnProperty(parsed, "type"));
+    const typ = /** @type {keyof typeof events} */ (parsed.type);
+    /**
+     * @template T
+     * @type {{ schema: import("ajv").JSONSchemaType<T> }}
+     */
+    const typeDef = events[typ];
+    t.assert(isValid(typeDef, parsed));
+  }
 });
